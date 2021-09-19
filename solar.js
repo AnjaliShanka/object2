14476467f226fd5963e9b26ba045afb83ff98091
AnjaliShanka <75622182+AnjaliShanka@users.noreply.github.com>
2021-09-19 11:54:00 +0530

    Add files via upload

---
+++ solar.js
@@ -0,0 +1,36 @@
+img = "";
+status1 = "";
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();   
+}
+
+function preload(){
+  img = loadImage('solar-system.jpg');  
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status1 = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
